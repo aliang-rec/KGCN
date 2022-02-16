953eb051cfbb4b65a7ae72e3e4082673d1719b97
linxingliang <linxingliang@fuzhi.ai>
2022-02-16 11:48:50 +0800

    push

--- src/main.py
+++ src/main.py
@@ -9,17 +9,17 @@ np.random.seed(555)
 
 parser = argparse.ArgumentParser()
 
-# movie
-parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
-parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
-parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
-parser.add_argument('--neighbor_sample_size', type=int, default=4, help='the number of neighbors to be sampled')
-parser.add_argument('--dim', type=int, default=32, help='dimension of user and entity embeddings')
-parser.add_argument('--n_iter', type=int, default=2, help='number of iterations when computing entity representation')
-parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
-parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
-parser.add_argument('--lr', type=float, default=2e-2, help='learning rate')
-parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
+# # movie
+# parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
+# parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
+# parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
+# parser.add_argument('--neighbor_sample_size', type=int, default=4, help='the number of neighbors to be sampled')
+# parser.add_argument('--dim', type=int, default=32, help='dimension of user and entity embeddings')
+# parser.add_argument('--n_iter', type=int, default=2, help='number of iterations when computing entity representation')
+# parser.add_argument('--batch_size', type=int, default=65536, help='batch size')
+# parser.add_argument('--l2_weight', type=float, default=1e-7, help='weight of l2 regularization')
+# parser.add_argument('--lr', type=float, default=2e-2, help='learning rate')
+# parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 
 '''
 # book
@@ -35,7 +35,7 @@ parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
 '''
 
-'''
+
 # music
 parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
@@ -47,7 +47,6 @@ parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
 parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
 parser.add_argument('--ratio', type=float, default=1, help='size of training dataset')
-'''
 
 
 show_loss = False
